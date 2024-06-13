8b7ae4a6fbf34e16fe2d2c3df5f6937b5ec2732f
Asti22 <uun3461@gmail.com>
2024-06-13 15:35:09 +0700

    jika data di fiel lebih besar taro di kanan parent

--- binaryseacrhtree.cpp
+++ binaryseacrhtree.cpp
@@ -42,7 +42,11 @@ public:
       }
       if (element < parent->info) //if the vaue in the data field of the new node is less than that of the parent
       {
-        parent->leftchild =newNode;
+        parent->leftchild =newNode; //make the left child of the parent point to the nwenode
+      }
+      else if (element>parent ->info ) //if the value  in the data field of the new node is greater than that of the parent
+      {
+        parent->rightchild =newNode; //maake the right child on the parentpoint to the newnode
       }
      }
 };
